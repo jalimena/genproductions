ddba194494b2b26c2ef1b808583919b1d6282ef0
aalbert <albert@physik.rwth-aachen.de>
2017-11-14 14:00:07 +0100

    Code style.

--- EXO-16-052/write_yaml.py
+++ EXO-16-052/write_yaml.py
@@ -5,28 +5,25 @@ import os
 from hepdata_lib import *
 
 
-
 def write_table_4(outdir):
-    #### This is the raw data from EXO-16-052
-    #### Table 4
-    #### It has four columns, corresponding to four variables
+    # This is the raw data from EXO-16-052
+    # Table 4
+    # It has four columns, corresponding to four variables
     data = [
-    [(100,125), 311 , (300,    18 ), ( 256,32   )],
-    [(125,150), 155 , (155.0, 7.0 ), ( 150,12   )],
-    [(150,175), 87  , (90.8, 4.6  ), ( 86.9,8.4 )],
-    [(175,200), 50  , (54.7, 3.1  ), ( 52.7,5.3 )],
-    [(200,250), 56  , (51.3, 2.9  ), ( 50.2,4.9 )],
-    [(250,300), 15  , (19.7, 1.4  ), ( 19.4,2.2 )],
-    [(300,350), 11  , (9.64, 0.80 ), ( 9.4,1.2  )],
-    [(350,400), 6   , (4.73, 0.47 ), ( 4.58,0.66)],
-    [(400,500), 6   , (3.44, 0.39 ), ( 3.31,0.54)],
-    [(500,13000), 1  , (1.63, 0.24 ), (1.57,0.33 )]
+        [(100, 125), 311, (300,    18), (256, 32)],
+        [(125, 150), 155, (155.0, 7.0), (150, 12)],
+        [(150, 175), 87, (90.8, 4.6), (86.9, 8.4)],
+        [(175, 200), 50, (54.7, 3.1), (52.7, 5.3)],
+        [(200, 250), 56, (51.3, 2.9), (50.2, 4.9)],
+        [(250, 300), 15, (19.7, 1.4), (19.4, 2.2)],
+        [(300, 350), 11, (9.64, 0.80), (9.4, 1.2)],
+        [(350, 400), 6, (4.73, 0.47), (4.58, 0.66)],
+        [(400, 500), 6, (3.44, 0.39), (3.31, 0.54)],
+        [(500, 13000), 1, (1.63, 0.24), (1.57, 0.33)]
     ]
-    
-    variables = []
 
     table = Table("table4")
-    ### First variable/column ---> MET
+    # First variable/column ---> MET
     met = Variable("MET")
     met.is_independent = True
     met.is_binned = True
@@ -34,71 +31,71 @@ def write_table_4(outdir):
     met.values_low = [item[0][0] for item in data]
     met.values_high = [item[0][1] for item in data]
     table.add_variable(met)
-    
-    ### Second variable/column ---> Number of observed events
+
+    # Second variable/column ---> Number of observed events
     obs = Variable("Observed")
     obs.is_independent = False
     obs.is_binned = False
     obs.units = "Events"
     obs.values = [item[1] for item in data]
     table.add_variable(obs)
-    
-    ### Third variable/column ---> Number of predicted events from full fit
+
+    # Third variable/column ---> Number of predicted events from full fit
     exp_full = Variable("Prediction (SR+CR fit)")
     exp_full.is_independent = False
     exp_full.is_binned = False
     exp_full.units = "Events"
     exp_full.values = [item[2][0] for item in data]
-    
+
     unc1 = Uncertainty("total")
     unc1.values = [item[2][1] for item in data]
     exp_full.uncertainties.append(unc1)
-    
+
     table.add_variable(exp_full)
-    
-    
-    ### Third variable/column ---> Number of predicted events from CR-only fit
+
+    # Third variable/column ---> Number of predicted events from CR-only fit
     exp_full = Variable("Prediction (CR-only fit)")
     exp_full.is_independent = False
     exp_full.is_binned = False
     exp_full.units = "Events"
     exp_full.values = [item[3][0] for item in data]
-    
+
     unc1 = Uncertainty("total")
     unc1.values = [item[3][1] for item in data]
     exp_full.uncertainties.append(unc1)
-    
+
     table.add_variable(exp_full)
 
     table.write_yaml(outdir)
 
+
 def write_submission_file(outdir):
-    #### Write submission file
+    # Write submission file
     submission = {}
     submission["name"] = "Table 4"
     submission["description"] = "A histogram we really care about. It is super important. That's why it has a description."
-    submission["keywords"] = [ {"name":"Analysis groups", "values":["EXO","EXO-METX"]} ]
+    submission["keywords"] = [
+        {"name": "Analysis groups", "values": ["EXO", "EXO-METX"]}]
     submission["data_file"] = "table4.yaml"
 
     if(not os.path.exists(outdir)):
         os.makedirs(outdir)
-    with open(os.path.join(outdir,'submission.yaml'), 'w') as outfile:
+    with open(os.path.join(outdir, 'submission.yaml'), 'w') as outfile:
         yaml.dump(submission, outfile, default_flow_style=False)
 
 
-
 def main():
-    ### Write to this directory
+    # Write to this directory
     outdir = "./submission/"
 
-    ### Write some files
+    # Write some files
     write_table_4(outdir)
     write_submission_file(outdir)
 
-    ### Put them into a tar file
+    # Put them into a tar file
     import tarfile
     tar = tarfile.open("submission.tar.gz", "w:gz")
-    for f in find_all_matching(outdir,"*.yaml"):
+    for f in find_all_matching(outdir, "*.yaml"):
         tar.add(f)
     tar.close()
 
--- lib/hepdata_lib.py
+++ lib/hepdata_lib.py
@@ -1,18 +1,23 @@
 import os
 import fnmatch
 import yaml
-def find_all_matching(path,pattern):
+
+
+def find_all_matching(path, pattern):
     if not os.path.exists(path):
         raise RuntimeError("Invalid path '{0}'".format(path))
     result = []
     for root, dirs, files in os.walk(path):
         for thisfile in files:
             if fnmatch.fnmatch(thisfile, pattern):
-                result.append(os.path.join(root, thisfile ))
+                result.append(os.path.join(root, thisfile))
     return result
 
 
-class Variable():
+class Variable(object):
+    # pylint: disable=too-many-instance-attributes
+    # Eight is reasonable in this case.
+
     def __init__(self, name):
         self.name = name
         self.is_independent = True
@@ -26,54 +31,57 @@ class Variable():
 
     def make_dict(self):
         tmp = {}
-        tmp["header"]={"name":self.name, "units" : self.units}
-        #~ tmp["qualifiers"]=[{ "name" : "TESTQUALNAME", "value":"TESTQUALVALUE" }]
+        tmp["header"] = {"name": self.name, "units": self.units}
 
-        tmp["values"]=[]
+        tmp["values"] = []
 
         for i in range(len(self.values_low if self.is_binned else self.values)):
             valuedict = {}
 
-            if(self.is_binned):
+            if self.is_binned:
                 valuedict["high"] = self.values_high[i]
-                valuedict["low"]  = self.values_low[i]
+                valuedict["low"] = self.values_low[i]
             else:
                 valuedict["value"] = self.values[i]
 
             for unc in self.uncertainties:
-                if( not "errors" in valuedict.keys() ): valuedict['errors'] = []
+                if "errors" not in valuedict.keys():
+                    valuedict['errors'] = []
                 if unc.is_symmetric:
-                    valuedict['errors'].append( { "symerror" : unc.values[i],
-                                                  "label" : unc.label})
+                    valuedict['errors'].append({"symerror": unc.values[i],
+                                                "label": unc.label})
                 else:
-                    valuedict['errors'].append( { "asymerror" : { "minus" : unc.values_down[i],
-                                                                  "plus" : unc.values_up[i]}})
+                    valuedict['errors'].append({"asymerror": {"minus": unc.values_down[i],
+                                                              "plus": unc.values_up[i]}})
             tmp["values"].append(valuedict)
         return tmp
 
-class Table():
-    def __init__(self,name):
+
+class Table(object):
+    def __init__(self, name):
         self.name = name
         self.variables = []
 
-    def add_variable(self,variable):
+    def add_variable(self, variable):
         self.variables.append(variable)
 
-    def write_yaml(self,outdir="."):
-        ### Put all variables together into a table and write
+    def write_yaml(self, outdir="."):
+        # Put all variables together into a table and write
         table = {}
         table["independent_variables"] = []
         table["dependent_variables"] = []
-        for v in self.variables:
-            table[ "independent_variables" if v.is_independent else "dependent_variables" ].append(v.make_dict())
+        for var in self.variables:
+            table["independent_variables" if var.is_independent else "dependent_variables"].append(
+                var.make_dict())
 
-        if(not os.path.exists(outdir)):
+        if not os.path.exists(outdir):
             os.makedirs(outdir)
-        with open(os.path.join(outdir,'{NAME}.yaml'.format(NAME=self.name)), 'w' ) as outfile:
+        with open(os.path.join(outdir, '{NAME}.yaml'.format(NAME=self.name)), 'w') as outfile:
             yaml.dump(table, outfile, default_flow_style=False)
 
-class Uncertainty():
-    def __init__(self,label):
+
+class Uncertainty(object):
+    def __init__(self, label):
         self.label = label
         self.is_symmetric = True
         self.values = []
