e4c550e76ca45638d2a8a17fea6d3e1559f60e8b
aalbert <albert@physik.rwth-aachen.de>
2017-12-15 13:48:40 +0100

    EXO-16-056: Fix order of uncertainties.

--- EXO-16-056/write_yaml.py
+++ EXO-16-056/write_yaml.py
@@ -39,18 +39,22 @@ def make_table_figure19(outdir):
     unc1.set_values(
         zip(
             reader.read_tree(
-                "xsecTree", "xsecULExpPlus_PFDijet2016"
+                "xsecTree", "xsecULExpMinus_PFDijet2016"
             ),
             reader.read_tree(
-                "xsecTree", "xsecULExpMinus_PFDijet2016"),
-        ),
-        nominal=exp.values)
+                "xsecTree", "xsecULExpPlus_PFDijet2016"
+            )
+            ), nominal=exp.values)
 
     unc2 = Uncertainty("2 s.d.")
     unc2.is_symmetric = False
-    unc2.set_values(zip(reader.read_tree(
-        "xsecTree", "xsecULExpPlus2_PFDijet2016"), reader.read_tree(
-        "xsecTree", "xsecULExpMinus2_PFDijet2016")), nominal=exp.values)
+    unc2.set_values(
+        zip(
+            reader.read_tree(
+                "xsecTree", "xsecULExpMinus2_PFDijet2016"
+            ), reader.read_tree(
+                "xsecTree", "xsecULExpPlus2_PFDijet2016"
+            )), nominal=exp.values)
 
     exp.uncertainties.append(unc1)
     exp.uncertainties.append(unc2)
